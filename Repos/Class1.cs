717dea0d769de4037a6237fc3cdfb0b0ec3facc1
Gabriel Kosteff Benith <60756730+GabrielBenith@users.noreply.github.com>
2020-03-10 22:43:00 -0300

    Add files via upload

---
+++ Repos/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public partial class Program
+{
+    static void Main(string[] avgs)
+    {
+        double n1, n2, n3, n4, n5;
+
+        Console.Write("Digite o Primeiro Número:");
+        n1 = double.Parse(Console.ReadLine());
+        Console.Write("Digite o Segundo Número:");
+        n2 = double.Parse(Console.ReadLine());
+        Console.Write("Digite o Terceiro Número:");
+        n3 = double.Parse(Console.ReadLine);
+        Console.Write("Digite O Quarto Número");
+        n4 = double.Parse(Console.ReadLine);
+        n5 = (n1 + n2 + n3 + n4) / 4;
+        Console.Write("A média é?" + n5.ToString);
+        Console.ReadKey();
+    }
+
+}
